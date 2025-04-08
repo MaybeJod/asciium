7e51665680047b4a134baba7a7fd301adbb4e965
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-08 15:03:42 +0200

    feat: trying to deploy, does not work

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
 import asciiRoutes from "./routes/asciiRoute.js";
 
@@ -8,6 +10,8 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+const __dirname = path.resolve();
 
 //middleware
 app.use(
@@ -22,6 +26,14 @@ app.use(
 app.use(express.json());
 
 app.use("/api/ascii", asciiRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(port, () => {
 	connectDB();
