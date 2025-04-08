c83da9288366c3dacd754861f2c73302ee031590
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-08 13:47:00 +0200

    feat: added delete and edit ascii

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,35 @@
+import { Link } from "react-router-dom";
+import { useAscii } from "@/contexts/AsciiContext";
+import { useEffect } from "react";
+import AsciiCard from "@/components/AsciiCard/AsciiCard";
+
 export default function HomePage() {
-	return <div>HomePage</div>;
+	const { fetchAsciis, asciis, loading, error } = useAscii();
+
+	useEffect(() => {
+		fetchAsciis();
+	}, []);
+
+	return (
+		<main className="m-19">
+			<h2>Current Ascii</h2>
+
+			{loading && <p>Loading...</p>}
+
+			{error && <p>Error: {error}</p>}
+
+			{!loading && asciis.length > 0 ? (
+				<section className="grid grid-cols-3 gap-3 ">
+					{asciis.map((ascii) => (
+						<AsciiCard key={ascii._id} ascii={ascii} />
+					))}
+				</section>
+			) : (
+				<div>
+					{!loading && <p>No Ascii found :(</p>}
+					<Link to="/create">Create new Ascii</Link>
+				</div>
+			)}
+		</main>
+	);
 }
