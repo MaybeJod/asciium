55d6dee9c58075620f5c287b7db7cc2d4bddc095
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-09 11:37:20 +0200

    oki

--- frontend/src/services/asciiService.tsx
+++ frontend/src/services/asciiService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:4000/api";
-const API_URL = "/api";
+const API_URL = "https://asciium.vercel.app/api";
 
 //create an axios instance
 const api = axios.create({
