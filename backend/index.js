c9aa1db89f62b3cc331a513511c9e3e31e1cbab1
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-10 15:09:43 +0200

    wip: does this work?

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(
 		origin:
 			process.env.NODE_ENV === "production"
 				? process.env.PRODUCTION_URL
-				: "http://localhost:5174",
+				: "http://localhost:5173",
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		credentials: true,
 	})
