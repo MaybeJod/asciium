320dad79997b2a1a8fad8deb578bf0115f8f01e1
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-11 14:24:47 +0200

    wip: back at it

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,12 @@ app.use(cors());
 app.use("/api/ascii", asciiRoutes);
 
 // console.log(process.env.NODE_ENV);
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(port, () => {
 	connectDB();
