02647f1a616ff8041b82a96d7fb2b96d659e50e5
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-11 14:09:28 +0200

    wip: yo its friday dont push do main

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
-const dirname = path.resolve();
+const __dirname = path.resolve();
 
 //middleware
 //allows us to accept json data in the req.body
@@ -22,8 +22,7 @@ app.use("/api/ascii", asciiRoutes);
 
 // console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(dirname, "/frontend/dist")));
-
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 	});
