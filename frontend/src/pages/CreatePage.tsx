a6f5ce53e064cba657a2ec6aa47af3a7d06b57f7
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-17 12:00:33 +0200

    wip: fix no need to refresh homepage after creating ascii

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -1,21 +1,12 @@
-import AsciiForm from "@/components/AsciiForm/AsciiForm";
-// import { AsciiProvider } from "@/contexts/AsciiContext";
-// import WebcamToAscii from "../components/CamToAscii/AsciiPage";
 import CamToAscii from "@/components/CamToAscii/CamToAscii";
-import { AsciiProvider } from "@/contexts/AsciiArtContext";
 
 export default function CreatePage() {
 	return (
 		<>
-			<AsciiProvider>
-				<div className="container mx-auto">
-					<h1 className="text-3xl font-bold">CamToAscii</h1>
-					<CamToAscii />
-				</div>
-				{/* <section className="flex justify-center">
-					<AsciiForm />
-				</section> */}
-			</AsciiProvider>
+			<div className="container mx-auto">
+				<h1 className="text-3xl font-bold">CamToAscii</h1>
+				<CamToAscii />
+			</div>
 		</>
 	);
 }
