a602c54c15ec65c79e75d23f73049db58613a5b1
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-17 17:57:59 +0200

    wip: use nav component from shadcn

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
 import { ModeToggle } from "../darkMode/mode-toggle";
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuList,
+	navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
 
 export default function Navbar() {
 	return (
-		<nav className="flex justify-evenly">
-			<Link to={"/"}>Home</Link>
-			<Link to={"/create"}>Create page</Link>
-			<ModeToggle />
-		</nav>
+		<NavigationMenu className="w-100 flex justify-between fixed z-50 bg-amber-400">
+			<NavigationMenuList>
+				<NavigationMenuItem>
+					<Link to="/" className={navigationMenuTriggerStyle()}>
+						Home
+					</Link>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<Link to="/Create" className={navigationMenuTriggerStyle()}>
+						Create Ascii
+					</Link>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<ModeToggle />
+				</NavigationMenuItem>
+			</NavigationMenuList>
+		</NavigationMenu>
 	);
 }
