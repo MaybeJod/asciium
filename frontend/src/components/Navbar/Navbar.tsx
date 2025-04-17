097a4c6d0fe59b8ae11ea5ca21e4bf462065581c
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-17 18:50:15 +0200

    wip: style nav

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -9,22 +9,37 @@ import {
 
 export default function Navbar() {
 	return (
-		<NavigationMenu className="w-100 flex justify-between fixed z-50 bg-amber-400">
-			<NavigationMenuList>
-				<NavigationMenuItem>
-					<Link to="/" className={navigationMenuTriggerStyle()}>
-						Home
+		<header className="border-grid fixed top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+			<div className="container mx-auto flex items-center justify-between px-1 py-2">
+				<div className="flex items-center">
+					<Link to="/" className="text-lg font-bold">
+						ASCIIUM
 					</Link>
-				</NavigationMenuItem>
-				<NavigationMenuItem>
-					<Link to="/Create" className={navigationMenuTriggerStyle()}>
-						Create Ascii
-					</Link>
-				</NavigationMenuItem>
-				<NavigationMenuItem>
-					<ModeToggle />
-				</NavigationMenuItem>
-			</NavigationMenuList>
-		</NavigationMenu>
+				</div>
+
+				<NavigationMenu>
+					<NavigationMenuList className="flex items-center gap-1">
+						<NavigationMenuItem>
+							<Link to="/" className={navigationMenuTriggerStyle()}>
+								Home
+							</Link>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<Link to="/Create" className={navigationMenuTriggerStyle()}>
+								Create Ascii
+							</Link>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<Link to="/github" className={navigationMenuTriggerStyle()}>
+								Github
+							</Link>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<ModeToggle />
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
+		</header>
 	);
 }
