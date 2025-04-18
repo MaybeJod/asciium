900227059cd3559a4613b1ee10d2c87d7d47cebd
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-18 12:45:39 +0200

    security: mongoose validation in ascii schema

--- backend/models/AsciiArtModel.js
+++ backend/models/AsciiArtModel.js
@@ -6,11 +6,13 @@ const AsciiArtSchema = new mongoose.Schema(
 		content: {
 			type: String,
 			required: true,
+			maxLength: [50000, "Content cannot exceed 50000 characters"],
 		},
 		//title
 		title: {
 			type: String,
 			require: true,
+			maxLength: [100, "Title cannot exceed 100 characters"],
 		},
 	},
 	{
