2f164b0630e22e5d44d07f22d3ad505013577a5d
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-18 14:00:21 +0200

    fix: added h1 in create page

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -4,6 +4,7 @@ export default function CreatePage() {
 	return (
 		<>
 			<div className="container mx-auto my-20">
+				<h1 className="text-8xl mb-5">Creation in progress</h1>
 				<CamToAscii />
 			</div>
 		</>
