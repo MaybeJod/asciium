9920ee52f7f6068eb9a2a22e68852372c02e7504
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-04-21 16:10:26 +0200

    fix: import path

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useMediaQuery } from "@/hooks/useMediaQuery";
-import DesktopNav from "./desktopNav";
-import MobileNav from "./mobileNav";
+import DesktopNav from "./DesktopNav";
+import MobileNav from "./MobileNav";
 
 export default function Navbar() {
 	const isDesktop = useMediaQuery("(min-width: 768px)");
